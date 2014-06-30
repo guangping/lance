fd9274145089c67499567bd584e537027ab70e3f
guangping <guangping.sony@gmail.com>
2014-06-30 08:01:51 +0000

    rop-ori-0.0.0.2

---
+++ rop-ori-0.0.0.2/src/main/sample/com/rop/service/SysOpenService.java
@@ -0,0 +1,30 @@
+package com.rop.service;
+
+import com.rop.annotation.NeedInSessionType;
+import com.rop.annotation.ServiceMethod;
+import com.rop.annotation.ServiceMethodBean;
+import com.rop.params.request.LogonRequest;
+import com.rop.params.response.LogonResponse;
+
+import java.util.UUID;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: guangping
+ * Date: 2014-06-27 15:24
+ * To change this template use File | Settings | File Templates.
+ */
+@ServiceMethodBean(version = "1.0")
+public class SysOpenService {
+
+    @ServiceMethod(method = "user.getSession", version = "1.0", needInSession = NeedInSessionType.NO)
+    LogonResponse getSession(LogonRequest request) {
+        LogonResponse response=new LogonResponse();
+        response.setResult(true);
+        response.setCode("0");
+        response.setMsg("success");
+        response.setSessionId(UUID.randomUUID().toString());
+
+        return response;
+    }
+}
---
+++ rop-ori-0.0.0.2/src/test/java/com/rop/sys/SysTest.java
@@ -0,0 +1,39 @@
+package com.rop.sys;
+
+import com.rop.client.CompositeResponse;
+import com.rop.client.DefaultRopClient;
+import com.rop.params.request.LogonRequest;
+import com.rop.params.response.LogonResponse;
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertNotNull;
+import static org.testng.Assert.assertTrue;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: guangping
+ * Date: 2014-06-27 16:36
+ * To change this template use File | Settings | File Templates.
+ */
+public class SysTest {
+    public static final String SERVER_URL = "http://localhost:8080/router";
+    public static final String APP_KEY = "00001";
+    public static final String APP_SECRET = "123";
+    private DefaultRopClient ropClient = new DefaultRopClient(SERVER_URL, APP_KEY, APP_SECRET);
+
+    @Test
+    public void createSession() {
+        LogonRequest ropRequest = new LogonRequest();
+        ropRequest.setUserName("tomson");
+        ropRequest.setPassword("123");
+        CompositeResponse response = ropClient.buildClientRequest()
+                .post(ropRequest, LogonResponse.class, "user.getSession", "1.0");
+        assertNotNull(response);
+        assertTrue(response.isSuccessful());
+        assertNotNull(response.getSuccessResponse());
+        assertTrue(response.getSuccessResponse() instanceof LogonResponse);
+        assertEquals(((LogonResponse) response.getSuccessResponse()).getSessionId(), "mockSessionId1");
+        ropClient.setSessionId(((LogonResponse) response.getSuccessResponse()).getSessionId());
+    }
+}
