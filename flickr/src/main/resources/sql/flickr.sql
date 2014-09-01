f807e83f5e37257d0ed70f87644dd8f90f776015
guangping <guangping.sony@gmail.com>
2014-09-01 02:45:27 +0000

    库设计脚本

---
+++ flickr/src/main/resources/sql/flickr.sql
@@ -0,0 +1,42 @@
+/*
+SQLyog Ultimate v8.8 
+MySQL - 5.5.5-10.0.12-MariaDB : Database - flickr
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`flickr` /*!40100 DEFAULT CHARACTER SET utf8 */;
+
+USE `flickr`;
+
+/*Table structure for table `sequence` */
+
+DROP TABLE IF EXISTS `sequence`;
+
+
+CREATE TABLE `sequence` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `stub` char(1) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `stub` (`stub`)
+) ENGINE=MyISAM AUTO_INCREMENT=20 DEFAULT CHARSET=utf8;
+
+
+SET   auto_increment_increment = 2;  
+SET   auto_increment_offset = 1;  
+ 
+REPLACE INTO sequence(stub) VALUES('a');
+SELECT LAST_INSERT_ID();
+
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
